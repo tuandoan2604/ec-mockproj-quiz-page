e94d4750f655e1b157fcf7b7354addfb5c9ad071
Kakashi944 <tranvietha3@gmail.com>
2022-06-19 22:20:16 +0700

    add audit log

--- README.md
+++ README.md
@@ -1,37 +1,30 @@
-# Quiz Page EC Backend
 
-## Project1 in EC,Please fork this repo and show us your development progress via a Pull Request.
+I. Run API 
+Dowload the the full folder, insall PostGreSQL DB. Creat a schema name "User", then create 3 table "User"( store user name, password and user ID), "Question" (store Question with choice A,B,C,D and the corrected answer). Password for current DB is Ha2101994. 
 
-I, Requirements
+To run the API, using command " go run main.go"
 
-	1. Support user registration function, login is required to perform the survey.
-	2. Once logged in, the first page displays the first question and the total number of questions to be
-		answered.
-	3. Click save and next to save the results and go to the next page, back to return to the previous
-		question (Optional: Support skip question).
-	4. Questions can choose 1 or more answers.
-	5. Show a list of selected questions and answers along with the correct answer and total score
-		after completing the survey.
-	6. Use Token Based Authentication with access token, refresh token.
-	7. Write Middleware for Authentication/Authorization actions.
-	8. Implement audit log for all actions.
-	9. Use PostGreSQL as database, can use any ORM for query.
-  
-II, Tools (Optional)
+II. Explain the API 
+1. Login 
 
-	1. ExpressJS
-	2. Middleware (Authen, Author, Validate data, Error Handle). Authen Author by JWT (Json Web Token)
-	3. Database PostGreSQL (use ORM Sequelize)
-	4. Audit log by winston
-	5. Swagger UI for Docs API 
-	
-IV, Write a concise README
+to Login, using send a Post request with Username and Password to "localhost:8080/login" (Username and password send in Post body using x-www-form-urlencode)
+API will answer with UserID and token, refresh token. Token will expire after 1 hour, after that, have to use refresh token. 
 
-	How to run your code locally?
-	A sample “curl” command to call your API
-	How to run your unit tests locally?
-	What do you love about your solution?
-	What else do you want us to know about however you do not have enough time to complete?
-  
-## Notes
-We recommend using Express JS, PostgreSQL. However, we encourage you to use the programming language that you are most comfortable with because we want you to shine with all your skills and knowledge.
+2. Get the question and choice
+send GET request to "localhost:8080/question/:number" (:number is the question number).
+ex: "localhost/question/1" to get the first question and the choice. 
+
+API will answer with Question and all the Choice
+
+3. Get the point of user 
+send GET request to "localhost/point/:id" (:id is the UserID, get UserID when user login)
+ex: "localhost:8080/point/:1" get the point of the user with UserID :1 
+
+4. To send the user answer 
+send Post request to "localhost/question/?id=&quest=&ans"
+ex: "localhost:8080/question/?id=1&quest=2&ans=A" is user (userID) 1 chose answer A of question 2
+
+5. To get the correct answer
+send GET request to "localhost:8080/question/ans" to get the correct answer of all the question. 
+
+all the action (send and get answer of API) will be store in the "out.log" file in the main folder. 
--- controller/Quiz.go
+++ controller/Quiz.go
@@ -6,13 +6,30 @@ import (
 	"Quiz/service"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 )
 
+var log1 = logrus.New()
+var log2 = logrus.New()
+
+func setupLog() {
+	log1.Out = os.Stderr
+	log1.Formatter = &logrus.TextFormatter{}
+
+	LogOutputFile, err := os.OpenFile("out.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		logrus.Fatalf("error opening file: %v", err)
+	}
+
+	log2.Out = LogOutputFile
+	log2.Formatter = &logrus.JSONFormatter{}
+}
 func CheckError(err error) {
 	if err != nil {
 		panic(err)
@@ -24,6 +41,7 @@ var loginController service.LoginController = service.LoginHandler(jwtService)
 
 //// Post Login ///
 func PostLogin(c *gin.Context) {
+	setupLog()
 	db, _ := model.ConnectDataBase()
 	Username := c.PostForm("username")
 	Password := c.PostForm("password")
@@ -46,6 +64,14 @@ func PostLogin(c *gin.Context) {
 		status = "success"
 
 		c.JSON(http.StatusOK, gin.H{"Status": status, "token": token, "refreshToken": refreshToken, "UserId": id})
+		log1.WithFields(logrus.Fields{
+			"UserID":   id,
+			"Username": Username,
+		}).Info(" User Login ")
+		log2.WithFields(logrus.Fields{
+			"UserID":   id,
+			"Username": Username,
+		}).Info(" User Login ")
 		//tocken = auth.JWTService.GenerateToken(auth.JWTService, Username, tru)
 	} else {
 		//token = ""
@@ -56,6 +82,7 @@ func PostLogin(c *gin.Context) {
 
 ////GET Question /////
 func GetQuestion(c *gin.Context) {
+	setupLog()
 	db, err := model.ConnectDataBase()
 	CheckError(err)
 
@@ -74,31 +101,45 @@ func GetQuestion(c *gin.Context) {
 
 		c.JSON(http.StatusOK, gin.H{"Question": Question, "Choice": gin.H{"A": A, "B": B}})
 	}
-
+	log1.WithFields(logrus.Fields{
+		"Question number": number,
+	}).Info(" User Login ")
+	log2.WithFields(logrus.Fields{
+		"Question number": number,
+	}).Info(" Get question and choice ")
 }
 
 ////GET the correct answer ////
 
 func GetQuestionAnswer(c *gin.Context) {
+	setupLog()
 	db, err := model.ConnectDataBase()
 	CheckError(err)
 
-	row, err := db.Query(`SELECT "Question", "Answer" FROM "User"."Question" `)
+	row, err := db.Query(`SELECT "Question", "Answer", "Id" FROM "User"."Question" `)
 	CheckError(err)
 
+	var question string
+	var answer string
+	var id int
 	for row.Next() {
-		var question string
-		var answer string
 
-		row.Scan(&question, &answer)
+		row.Scan(&question, &answer, &id)
 
 		c.JSON(http.StatusOK, gin.H{"Question": question, "Answer": answer})
 	}
+	log1.WithFields(logrus.Fields{
+		"Question number": id,
+	}).Info(" User Login ")
+	log2.WithFields(logrus.Fields{
+		"Question number": id,
+	}).Info(" Get question and corrected answer ")
 }
 
 ////GET Point of User ////
 
 func GetPoint(c *gin.Context) {
+	setupLog()
 	db, err := model.ConnectDataBase()
 	CheckError(err)
 	userID := c.Param("id")
@@ -116,22 +157,32 @@ func GetPoint(c *gin.Context) {
 	}
 	fmt.Println("point: ", point)
 	c.JSON(http.StatusOK, gin.H{"UserId": userID, "Point": point})
+	log1.WithFields(logrus.Fields{
+		"UserID": userID,
+		"Point":  point,
+	}).Info(" User Login ")
+	log2.WithFields(logrus.Fields{
+		"UserID": userID,
+		"Point":  point,
+	}).Info(" Get Point of UserID ")
+
 }
 
 ////Post Answer from User ////
 
 func PostQuestion(c *gin.Context) {
+	setupLog()
 	db, err := model.ConnectDataBase()
 	CheckError(err)
 
-	userId := c.Query("id")
+	userID := c.Query("id")
 	question, _ := strconv.Atoi(c.Query("quest"))
 	answer := c.Query("ans")
 	time := time.Now().Format("2006-01-02 15:04:05")
 
 	var point int
 	//time := time.Now().Format("2020.16.06 14:29:00")
-	fmt.Println(userId, question, answer, time)
+	fmt.Println(userID, question, answer, time)
 
 	row, _ := db.Query(`SELECT "Answer" FROM "User"."Question" WHERE "Id" = $1`, question)
 
@@ -147,7 +198,7 @@ func PostQuestion(c *gin.Context) {
 		}
 	}
 
-	row, err = db.Query(`SELECT "UserId", "Question" FROM "User"."UserAnswer" WHERE "UserId" = $1 AND "Question" = $2`, userId, question)
+	row, err = db.Query(`SELECT "UserId", "Question" FROM "User"."UserAnswer" WHERE "UserId" = $1 AND "Question" = $2`, userID, question)
 	CheckError(err)
 	b := 0
 	for row.Next() {
@@ -158,14 +209,25 @@ func PostQuestion(c *gin.Context) {
 
 	if b != 0 {
 		update := `UPDATE "User"."UserAnswer" SET "Answer" = $1, "TimeStamp" = $2, "Point" =$3 WHERE "UserId" = $4 AND "Question" = $5 `
-		_, e := db.Exec(update, answer, time, point, userId, question)
+		_, e := db.Exec(update, answer, time, point, userID, question)
 		CheckError(e)
 	} else {
-		insert := `insert into "User"."UserAnswer" ("UserId", "Question", "Answer", "TimeStamp", "Point") values($1,$2,$3,$4,$5)`
-		_, e := db.Exec(insert, userId, question, answer, time, point)
+		insert := `insert into "User"."UserAnswer" ("UserID", "Question", "Answer", "TimeStamp", "Point") values($1,$2,$3,$4,$5)`
+		_, e := db.Exec(insert, userID, question, answer, time, point)
 		CheckError(e)
 	}
 
-	fmt.Println(userId, question, answer, time, point)
+	fmt.Println(userID, question, answer, time, point)
+
+	log1.WithFields(logrus.Fields{
+		"UserID":   userID,
+		"Question": question,
+		"Answer":   answer,
+	}).Info(" User Login ")
+	log2.WithFields(logrus.Fields{
+		"UserID":   userID,
+		"Question": question,
+		"Answer":   answer,
+	}).Info(" User sumbit aswer ")
 
 }
